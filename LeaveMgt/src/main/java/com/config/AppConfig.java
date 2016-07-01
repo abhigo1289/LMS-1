9158c7911173b90abe2de737c6b7636a84a3f957
abhigo1289 <abhishek.abhi1289@gmail.com>
2016-07-01 09:52:21 +0530

    done

--- LeaveMgt/src/main/java/com/config/AppConfig.java
+++ LeaveMgt/src/main/java/com/config/AppConfig.java
@@ -23,7 +23,7 @@ public class AppConfig {
 	public DriverManagerDataSource dataSource() {
 	    DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
 	    driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-	    driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/test");
+	    driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/LEAVEMANAGEMENT");
 	    driverManagerDataSource.setUsername("root");
 	    driverManagerDataSource.setPassword("abhi");
 	    return driverManagerDataSource;
